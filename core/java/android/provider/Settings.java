3e62ba63ee6ed940a8a1b31218cd371605b34c73
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    [SQUASH] base: Allow using 4G icon instead of LTE [1/2]
    [jhonboy121]: adapt for A13
    
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    2nd commit:
    
    SystemUI: improve 4G icon switch code
    
    * adapted to A12 L and A13
    * added setting key to backup array
    * use a single settings observer and use existing mReceiverHandler instead of creating new one's
    * keep the settings observer class private
    * don't update configuration when registering observer
    * only notify listeners if necessary
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    3rd commit:
    
    SystemUI: MobileSignalController: register 4G icon settings observer for all users
    
    * update settings on user switch as well
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5736,6 +5736,12 @@ public final class Settings {
         public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
 
         /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
