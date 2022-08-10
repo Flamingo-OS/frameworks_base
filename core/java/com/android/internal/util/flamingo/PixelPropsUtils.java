e57d5e91e173f343a4871ecb38bda8f99396ed93
jhenrique09 <jhsv09@gmail.com>
2022-08-10 15:28:39 +0000

    PixelPropsUtils: Update raven fingerprint
    Change-Id: Iace45db9376367326aadd770f09986a534f44e35

--- core/java/com/android/internal/util/flamingo/PixelPropsUtils.java
+++ core/java/com/android/internal/util/flamingo/PixelPropsUtils.java
@@ -50,7 +50,7 @@ public final class PixelPropsUtils {
         entry("DEVICE", "raven"),
         entry("PRODUCT", "raven"),
         entry("MODEL", "Pixel 6 Pro"),
-        entry("FINGERPRINT", "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys")
+        entry("FINGERPRINT", "google/raven/raven:12/SQ3A.220705.004/8836240:user/release-keys")
     );
 
     private static final Map<String, String> marlinProps = Map.ofEntries(
