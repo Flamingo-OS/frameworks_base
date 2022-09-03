d864b1417c1357d911b075df613c7d9e5e6dd32c
jhonboy121 <alfredmathew05@gmail.com>
2022-09-03 15:08:19 +0530

    SystemUI: do not block any statusbar icons
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarViewController.java
@@ -340,11 +340,11 @@ public class KeyguardStatusBarViewController extends ViewController<KeyguardStat
         }
         mView.setOnApplyWindowInsetsListener(
                 (view, windowInsets) -> mView.updateWindowInsets(windowInsets, mInsetsProvider));
-        mSecureSettings.registerContentObserverForUser(
+        /*mSecureSettings.registerContentObserverForUser(
                 Settings.Secure.getUriFor(Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON),
                 false,
                 mVolumeSettingObserver,
-                UserHandle.USER_ALL);
+                UserHandle.USER_ALL);*/
         updateUserSwitcher();
         onThemeChanged();
     }
@@ -356,7 +356,7 @@ public class KeyguardStatusBarViewController extends ViewController<KeyguardStat
         mUserInfoController.removeCallback(mOnUserInfoChangedListener);
         mStatusBarStateController.removeCallback(mStatusBarStateListener);
         mKeyguardUpdateMonitor.removeCallback(mKeyguardUpdateMonitorCallback);
-        mSecureSettings.unregisterContentObserver(mVolumeSettingObserver);
+        //mSecureSettings.unregisterContentObserver(mVolumeSettingObserver);
         if (mTintedIconManager != null) {
             mStatusBarIconController.removeIconGroup(mTintedIconManager);
         }
