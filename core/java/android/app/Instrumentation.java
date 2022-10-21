147b7bacaf9611ce1395b817d084c12352038482
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-10-21 09:58:27 +0300

    gmscompat: improve shim for background activity starts

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1814,7 +1814,7 @@ public class Instrumentation {
             checkStartActivityResult(result, intent);
 
             if (GmsCompat.isEnabled()) {
-                GmsHooks.onActivityStart(result, intent, options);
+                GmsHooks.onActivityStart(result, intent, requestCode, options);
             }
 
         } catch (RemoteException e) {
